d4f5ae9991b7fdfb9ce8c9b3ed548f4936221e9f
RotemLibrati <rotemlibrati1@gmail.com>
2022-04-03 18:56:12 +0300

    Create donation schema

--- donation-finder-nodejs/models/donation.js
+++ donation-finder-nodejs/models/donation.js
@@ -3,7 +3,9 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const donationSchema = new Schema({
-    name: { type: String, required: true }
+    creator: { type: String, required: true },
+    description: { type: String, required: true, minlength:6 },
+    typeDonation : {type: String, required: true }
 });
 
 module.exports = mongoose.model("donations", donationSchema);
